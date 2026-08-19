d14e0ad8da361a3269698ddf404f451cc3f753c6
Maryam Sy <meriemsy3@gmail.com>
2026-08-19 14:57:43 +0000

    test: mock ProductService in catalog test

--- src/app/features/catalog/catalog.spec.ts
+++ src/app/features/catalog/catalog.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { Catalog } from './catalog';
+import { ProductService } from '../../core/services/product.service';
 
 describe('Catalog', () => {
   let component: Catalog;
   let fixture: ComponentFixture<Catalog>;
 
+  const productServiceMock = {
+    getAll: () => of([]),
+    getCategories: () => of([]),
+    getByCategory: () => of([]),
+    getById: () => of({}),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Catalog],
+      providers: [
+        {
+          provide: ProductService,
+          useValue: productServiceMock,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Catalog);
